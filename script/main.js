1a88bb7cb6e800db306e1733ecd0ac07a4b14a1f
practicalRope <hkmelaa@hotmail.com>
2018-11-10 19:39:56 +0100

    hah

---
+++ script/main.js
@@ -0,0 +1,40 @@
+
+// let nodes = document.querySelectorAll('.artist');
+
+// console.log(nodes);
+
+// window.onkeydown = k =>{
+//     if(k.key === ' ') nodes.forEach((n,i) =>{ i ? n.style.display = 'none' : null; })
+// }
+
+// let currentShown = 0;
+
+
+
+// function decrement(){
+//     if(currentShown === 0) return;
+
+//     hide(currentShown);
+//     currentShown -= 1;
+//     show(currentShown);
+// }
+
+// function increment(){
+//     if(currentShown === nodes.length-1) return;
+
+//     show(currentShown);
+//     currentShown += 1;
+//     hide(currentShown);
+// }
+
+// function hide(idx){
+//     let tmp = nodes[idx].getElementsByTagName('iframe')
+    
+    
+//     nodes[idx].style.display = 'none';
+// }
+
+// function show(idx){
+//     nodes[idx].style.removeProperty('display');
+// }
+
